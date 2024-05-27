eb775c6dc814f5fca5f49757e76173a684df758d
swati2029 <swati.dodekar@gmail.com>
2024-05-27 19:01:50 +0530

    Add files via upload

--- demo.R
+++ demo.R
@@ -26,7 +26,83 @@ rm(tvu)
 Sub = 'english'  
 class(sub)  
 
+1+2
+  
+ hade (bank, 2) 
+  table = datasets::euro
+  view (euro)
+  
+  s =datasets::airmiles
+  
+  datasets::airmiles
+tail(s,5)
+  summary(s)
+  s
+  
+ t = F 
+class(t) 
 
-  
-  
-  
+df
+df$Month
+df$Solar.R
+summary(df$Day)
+attach(df)
+Temp
+######Scatter plot #########
+
+plot(x=Wind , y = Temp)
+
+plot(Ozone,Solar.R,xlab = 'OZONE LEVEL' ,ylab ='SOLAR' ,                    
+main = 'Scatter plot Betn ozone and solar',col = '#c842f5' , pch = 18
+     ) 
+
+attach (DailyTemps)
+
+plot (MaxTemp  , AvgTemp , xlab ='Maximium Temp' ,ylab='Avrage Temp' ,
+      ain="  Avge Temp", col ='Blue' , pch =19)
+plot(DailyTemps)
+
+plot(DailyTemps , col = 'green', pch = 16)
+
+#########Bar Plot ############
+
+attach(df)
+##Horizontal Bar Plot
+barplot(Month)
+table(Month)
+unique(Month)
+Frequency <-table(Month)
+barplot(Frequency , col = "yellow")
+barplot(table(Month))
+
+rm(Frequency)
+
+######Histogram########
+attach(df)
+hist(Wind , col = "brown")
+
+##### single Box Plot #####
+attach(df)
+boxplot(Ozone , col = 'white')
+boxplot(Ozone)$ stats
+boxplot(Ozone)$ out
+
+#MULTIPAL Box plot
+boxplot(df)
+
+########Multipal Graphs in one Canvas#####
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
