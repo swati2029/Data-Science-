cf9613587a2c63ba0caeee1bea29940d03210370
swati2029 <swati.dodekar@gmail.com>
2024-05-27 17:51:19 +0530

    Add files via upload

---
+++ demo.R
@@ -0,0 +1,32 @@
+1223
+23+22
+
+number = 914691
+class(number)
+
+age = 23 
+class(age)
+
+name = 'swati'
+class(name)
+
+
+s = F
+class(s)
+age = 21,22,23 
+age =  c(21,22,34,45,70)
+
+View(bank) 
+tuv=datasets::BOD
+
+
+rm(tvu)  
+  number +2
+  
+Sub = 'english'  
+class(sub)  
+
+
+  
+  
+  
